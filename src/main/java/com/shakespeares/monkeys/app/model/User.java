7fc38985a78793af9b5f7eb7159b78ae7a15c4d9
Pat Alwell <pat.alwell@gmail.com>
2022-05-28 13:27:20 -0700

    added admin user and role for actuator

--- src/main/java/com/shakespeares/monkeys/app/model/User.java
+++ src/main/java/com/shakespeares/monkeys/app/model/User.java
@@ -27,7 +27,6 @@ public class User {
 
 	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
 	@JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
-
 	private Collection<Role> roles;
 
 	public User() {
